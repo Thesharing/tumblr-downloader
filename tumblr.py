82ace92160166dbaf639d087a064723fdddfb04f
Thesharing <cyprestar@outlook.com>
2018-12-06 01:42:27 +0800

    [Fix] Auto retry when meet RequestException in client requests

--- tumblr.py
+++ tumblr.py
@@ -4,11 +4,13 @@ from math import ceil
 
 import yaml
 import requests
-import pytumblr
-from bs4 import BeautifulSoup as bs
+from pytumblr import TumblrRestClient
+from bs4 import BeautifulSoup as Soup
 from requests.exceptions import RequestException
 
 from util import oauth
+
+MAX_RETRY = 20
 
 
 class TumblrDownloader:
@@ -36,11 +38,11 @@ class TumblrDownloader:
         logging.info('Tokens ready')
 
         # Init client
-        self.client = pytumblr.TumblrRestClient(
-            tokens['consumer_key'],
-            tokens['consumer_secret'],
-            tokens['oauth_token'],
-            tokens['oauth_token_secret']
+        self.client = TumblrClient(
+            consumer_key=tokens['consumer_key'],
+            consumer_secret=tokens['consumer_secret'],
+            oauth_token=tokens['oauth_token'],
+            oauth_secret=tokens['oauth_token_secret']
         )
 
         # Get user info
@@ -63,7 +65,7 @@ class TumblrDownloader:
         """
         count = 0
         total = self.user_info['likes']
-        logging.info('(Likes) {0} ongoing'.format(total))
+        logging.info('Likes | {0} ongoing'.format(total))
 
         for page in range(start, ceil(total / 20)):
             logging.info('Downloading page {0}'.format(page))
@@ -71,16 +73,16 @@ class TumblrDownloader:
             for post in self.client.likes(limit=20, offset=page * 20)['liked_posts']:
 
                 if len(post['trail']) > 1 and not self.reblog:
-                    logging.info('(Skip) Reblog post: {0} | {1}'.format(post['blog_name'], post['post_url']))
+                    logging.info('Skip | Reblog post: {0} | {1}'.format(post['blog_name'], post['post_url']))
 
                 else:
-                    logging.info('(Blog) Start post {0} | {1}'.format(post['blog_name'], post['post_url']))
+                    logging.info('Likes | Start post {0} | {1}'.format(post['blog_name'], post['post_url']))
 
                     self._download_post(post)
 
                     count += 1
 
-        logging.info('(Likes) {0} / {1} downloaded'.format(count, total))
+        logging.info('Likes | {0} / {1} downloaded'.format(count, total))
 
     def download_following(self, start=0):
         """
@@ -89,19 +91,18 @@ class TumblrDownloader:
         """
         count = 0
         total = self.user_info['following']
-        logging.info('(Following) {0} blogs total'.format(total))
+        logging.info('Following | {0} blogs total'.format(total))
 
         for page in range(start, ceil(total / 20)):
-            logging.info('(Following) Page {0} ongoing'.format(page))
+            logging.info('Following | Page {0} ongoing'.format(page))
 
             for blog in self.client.following(limit=20, offset=page * 20)['blogs']:
-
-                logging.info('(Following) Start blog {0} | {1}'.format(blog['name'], blog['url']))
+                logging.info('Following | Start blog {0} | {1}'.format(blog['name'], blog['url']))
 
                 self.download_blog(blog['name'])
                 count += 1
 
-        logging.info('(Following) {0} / {1} blogs downloaded'.format(count, total))
+        logging.info('Following | {0} / {1} blogs downloaded'.format(count, total))
 
     def download_blog(self, blog_identifier, start=0):
         """
@@ -110,24 +111,26 @@ class TumblrDownloader:
         :param start: page number to start
         """
         count = 0
-        total = self.client.posts(blog_identifier)['total_posts']
-        logging.info('(Blog) {0} posts total'.format(total))
+        total = self.client.posts(blogname=blog_identifier)['total_posts']
+        logging.info('Blog | {0} posts total'.format(total))
 
         for page in range(start, ceil(total / 20)):
-            logging.info('(Blog) Page {0} ongoing'.format(page))
+            logging.info('Blog | Page {0} ongoing'.format(page))
 
             for post in self.client.posts(blogname=blog_identifier, limit=20, offset=page * 20)['posts']:
 
                 if len(post['trail']) > 1 and not self.reblog:
-                    logging.info('(Skip) Reblog post: {0} | {1}'.format(post['blog_name'], post['post_url']))
+                    logging.info('Skip | Reblog post: {0} | {1}'.format(
+                        post['post_url'].split('/')[-2], post['post_url']))
 
                 else:
-                    logging.info('(Blog) Start post {0} | {1}'.format(post['blog_name'], post['post_url']))
+                    logging.info('Blog | Start post {0} | {1}'.format(
+                        post['post_url'].split('/')[-2], post['post_url']))
 
                     self._download_post(post)
                     count += 1
 
-        logging.info('(Blog) {0} / {1} posts downloaded'.format(count, total))
+        logging.info('Blog | {0} / {1} posts downloaded'.format(count, total))
 
     def _download_post(self, post):
         """
@@ -149,7 +152,7 @@ class TumblrDownloader:
 
         elif post['type'] == 'text':
             raw = post['body']
-            soup = bs(raw, 'lxml')
+            soup = Soup(raw, 'lxml')
 
             # Find all images
             for img in soup.find_all('img'):
@@ -175,22 +178,62 @@ class TumblrDownloader:
             try:
                 r = requests.get(url, timeout=10)
             except RequestException:
-                logging.error('(Error) | {0} | {1}'.format(file_name, url))
+                logging.error('Error | {0} | {1}'.format(file_name, url))
                 continue
 
             if r.status_code > 400:
-                logging.error('({0}) | {1} | {2}'.format(r.status_code, file_name, url))
+                logging.error('{0} | {1} | {2}'.format(r.status_code, file_name, url))
                 return False
 
             elif r.status_code != 200:
-                logging.warning('({0}) | {1} | {2}'.format(r.status_code, file_name, url))
+                logging.warning('{0} | {1} | {2}'.format(r.status_code, file_name, url))
                 continue
 
             else:
-                logging.info('(OK) | {0} | {1}'.format(file_name, url))
+                logging.info('Download | {0} | {1}'.format(file_name, url))
                 with open(os.path.join(self.download_folder, file_name), 'wb') as f:
                     f.write(r.content)
                 return True
+
+
+class TumblrClient:
+    def __init__(self, **kwargs):
+        self.client = TumblrRestClient(**kwargs)
+        self.dummy = {
+            'likes': {'liked_posts': []},
+            'following': {'blogs': []},
+            'posts': {'total_posts': 0, 'posts': []}
+        }
+
+    def info(self):
+        return self.client.info()
+
+    def posts(self, **kwargs):
+        retry = MAX_RETRY
+        while retry:
+            try:
+                return self.client.posts(**kwargs)
+            except RequestException:
+                retry -= 1
+        return self.dummy['posts']
+
+    def following(self, **kwargs):
+        retry = MAX_RETRY
+        while retry:
+            try:
+                return self.client.following(**kwargs)
+            except RequestException:
+                retry -= 1
+        return self.dummy['following']
+
+    def likes(self, **kwargs):
+        retry = MAX_RETRY
+        while retry:
+            try:
+                return self.client.likes(**kwargs)
+            except RequestException:
+                retry -= 1
+        return self.dummy['likes']
 
 
 class BlogName:
